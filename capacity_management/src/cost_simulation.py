25d4b4a6b56f70967f44971915ae8f19445db082
Kaleigh Mentzer <kmentzer@stanford.edu>
2022-04-11 10:18:29 -0700

    cost simulation refactor to class and evaluation metrics

--- capacity_management/src/cost_simulation.py
+++ capacity_management/src/cost_simulation.py
@@ -2,206 +2,207 @@ import numpy as np
 from scipy.stats import binom
 
 
-def simulate_yield_costs(
-    true_cap: int, inf_cap: int, co: float, cu: float, iters: int, prob: float
-) -> np.ndarray:
-    """
-    Function that estimates the true cost corresponding to the
-    first school for a given true capacity and inflated capacity.
-    Returns the cost as an array with the first entry as the
-    overage and the second one as the underage cost.
+class CostSimulator:
+    def __init__(self, co: float, cu: float, iters: int, prob: float, true_caps: np.ndarray = None, inf_strategy: str = None):
+        self.true_caps = true_caps
+        self.co = co
+        self.cu = cu
+        self.iters = iters
+        self.prob = prob
+        self.allowed_strategies = ['chain', 'independent']
+        if inf_strategy is not None and inf_strategy not in self.allowed_strategies:
+            raise ValueError(f"Inflation strategy not recognized, please use one of {self.allowed_strategies}")
+        self.inf_strategy = inf_strategy
 
-    :param true_cap: true capacity of the school
-    :param inf_cap: inflated capacity
-    :param co: per unit cost of overage
-    :param cu: per unit cost of underage
-    :param iters: number of iterations
-    :param prob: probability of dropout
-    :return: len 2 array where first entry is overage cost and second is underage for the one school
-    """
-    yld = np.full(iters, inf_cap, dtype=float) - np.random.binomial(
-        inf_cap, prob, size=iters
-    )
-    diff = yld - np.full(iters, true_cap, dtype=float)
-    overage = co * sum(np.where(diff > 0, diff, 0)) / iters
-    underage = cu * sum(np.where(-1 * diff > 0, -1 * diff, 0)) / iters
-    costs = np.array([overage, underage])
-    return costs
+    def simulate_yield_costs(
+            self, true_cap: int, inf_cap: int
+    ) -> np.ndarray:
+        """
+        Function that estimates the true cost corresponding to the
+        first school for a given true capacity and inflated capacity.
+        Returns the cost as an array with the first entry as the
+        overage and the second one as the underage cost.
+
+        :param true_cap: true capacity of the school
+        :param inf_cap: inflated capacity
+        :return: len 2 array where first entry is overage cost and second is underage for the one school
+        """
+        yld = np.full(self.iters, inf_cap, dtype=float) - np.random.binomial(
+            inf_cap, self.prob, size=self.iters
+        )
+        diff = yld - np.full(self.iters, true_cap, dtype=float)
+        overage = self.co * sum(np.where(diff > 0, diff, 0)) / self.iters
+        underage = self.cu * sum(np.where(-1 * diff > 0, -1 * diff, 0)) / self.iters
+        costs = np.array([overage, underage])
+        return costs
+
+    def simulate_2_school_costs(
+            self,
+            true_cap1: int,
+            true_cap2: int,
+            inf_cap1: int,
+            inf_cap2: int,
+    ) -> np.ndarray:
+        """
+        Function that estimates the true total cost corresponding to the system
+        of two schools for some decisions on the inflated capacities.
+        Returns the cost as an array with the first entry as the
+        overage and the second one as the underage cost.
+
+        :param true_cap1: true capacity of the first school
+        :param inf_cap1: inflated capacity at the first school
+        :param true_cap2: true capacity of the second school
+        :param inf_cap2: inflated capacity at the second school
+        :return: len 2 array where first entry is overage cost and second is underage over both schools
+        """
+        sch1_cost = self.simulate_yield_costs(true_cap1, inf_cap1)
+        sch2_cost = self.simulate_yield_costs(true_cap2, inf_cap2)
+        return sch1_cost + sch2_cost
+
+    def optimize_2sch(
+            self, true_cap1: int, true_cap2: int,
+    ):
+        """
+        Function that performs a grid search optimizing for the best
+        pair of inflated capacities in terms of minimum total cost
+        to the system.
+
+        :param true_cap1: true capacity of the first school
+        :param true_cap2: true capacity of the second school
+        :return: np.ndarray containing costs from all inflated capacities in [q, q + qp/(1-p)]
+        """
+
+        # define intervals of possible amounts by which we can inflate by
+        # using qhat / (1-prob) <= q
+        int_length1 = int(true_cap1 * self.prob / (1 - self.prob))
+        int_length2 = int(true_cap2 * self.prob / (1 - self.prob))
+
+        # values we try for inflated capacities
+        values1 = np.arange(true_cap1, true_cap1 + int_length1 + 1)
+        values2 = np.arange(true_cap2, true_cap2 + int_length2 + 1)
+        avg_costs = np.zeros(shape=(int_length1+1, int_length2+1))
+
+        for qhat1 in values1:
+            for qhat2 in values2:
+                costs = self.simulate_2_school_costs(
+                    true_cap1, true_cap2, qhat1, qhat2
+                )
+                avg_costs[qhat1 - true_cap1, qhat2 - true_cap2] = costs[0] + costs[1]
+
+        return avg_costs
+
+    def heuristic_set_capacity(self, total_cap: int):
+        """
+        Function that computes the total inflation given the heuristic we use
+        to construct a long binomial and look for the approximate inverse
+        of the CDF for the value equal to the critical ratio.
+        Returns an integer, the total inflation.
+
+        :param total_cap: total capacity we are computing overage-underage costs with respect to
+        """
+
+        int_length = int(total_cap * self.prob / (1 - self.prob))
+        values = np.arange(total_cap, total_cap + int_length + 1)
+        ratio = float(self.co) / (self.cu + self.co)
+
+        best_val = values[0]
+        for qhat in values:
+            if binom.cdf(total_cap, qhat, 1 - self.prob) >= ratio:
+                best_val = qhat
+            else:
+                break
+        return best_val
+
+    def compute_cdf_inflate_sep(
+            self, cap1: int, cap2: int,
+    ) -> float:
+        """
+        Function that computes the CDF corresponding to the combined binomial,
+        resulting from adding up the inflated capacities, at the sum of true capacities.
+        Used to compare whether inflating separately results in more inflation than
+        when we are inflating school 2, school of interest, by viewing is as
+        integrated in the chain, as opposed to the top of its own chain or separately.
+        Returns a float, the value of CDF at the desired point.
+
+        :param cap1: capacity corresponding to the first binomial, so  either the first school or the chain up to our
+            school of interest
+        :param cap2: capacity
+        :return: float with the value of the CDF at the desired point
+        """
+
+        # compute the inflated capacities when first school/chain and the second
+        # school-school of interest-inflate separately
+        qhat1 = self.heuristic_set_capacity(cap1)
+        qhat2 = self.heuristic_set_capacity(cap2)
+
+        # compute CDF at the sum of true capacities
+        return binom.cdf(cap1 + cap2, qhat1 + qhat2, 1 - self.prob)
+
+    def simulate_multi_school_costs(self, inf_caps):
+        """
+        Function that estimates the true total costs corresponding to each of the
+        n schools for some decisions on the inflated capacities. Returns the result
+        as an n x 2 matrix, with the overage and underage cost corresponding to
+        each school as its rows.
+
+        :param inf_caps: inflated capacities at of each school
+        """
+
+        n = len(self.true_caps)
+        costs = np.zeros((n, 2), dtype=float)
+
+        yld = np.random.binomial(inf_caps, 1 - self.prob, size=(self.iters, n)).T
+        diff = yld - np.tile(self.true_caps, (self.iters, 1)).T
+
+        underfill_so_far = np.zeros(self.iters)
+        moving_up_total = np.zeros(self.iters)
+        for k in range(n):
+            underfilled_mask = np.where(diff[k, :] < underfill_so_far, 1, 0)
+            overfilled_mask = np.where(diff[k, :] > underfill_so_far, 1, 0)
+            costs[k, 0] += self.co * np.sum(overfilled_mask * (diff[k, :] - underfill_so_far))
+            costs[k, 1] += self.cu * np.sum(underfilled_mask * ((-1) * diff[k, :] + underfill_so_far))
+            remaining_underfill = underfill_so_far - overfilled_mask * diff[k, :] + underfilled_mask * (-1) * diff[k, :]
+            moving_up_total += overfilled_mask * np.minimum(diff[k, :], underfill_so_far)
+            underfill_so_far = np.where(remaining_underfill > 0, remaining_underfill, 0)
+
+        self.costs = costs / self.iters
+        self.underfill = underfill_so_far
+        self.movers = moving_up_total
+
+    def heuristic_set_capacity_chain(self):
+        running_true_cap = 0
+        running_inf_cap = 0
+        inf_caps = []
+        for q in self.true_caps:
+            running_true_cap += q
+            qhat = self.heuristic_set_capacity(running_true_cap) - running_inf_cap
+            running_inf_cap += qhat
+            inf_caps.append(qhat)
+        return np.array(inf_caps)
+
+    def heuristic_set_capacity_independent(self):
+        inf_caps = []
+        for q in self.true_caps:
+            qhat = self.heuristic_set_capacity(q)
+            inf_caps.append(qhat)
+        return np.array(inf_caps)
+
+    def evaluation_metrics(self, inf_caps):
+        metrics = {}
+        metrics["total_cost"] = np.sum(self.costs)
+        metrics["school_costs"] = np.sum(self.costs, axis=1)
+        metrics["school_overage_costs"] = self.costs[:, 0]
+        metrics["school_underage_costs"] = self.costs[:, 1]
+        metrics["avg_underfill"] = np.mean(self.underfill)
+        metrics["raw_inflation"] = inf_caps - self.true_caps
+        metrics["pct_inflation"] = metrics["raw_inflation"] / self.true_caps
+        metrics["avg_movers"] = np.mean(self.movers)
+        return metrics
+
+    def simulate(self):
+        capacity_func = getattr(self, f"heuristic_set_capacity_{self.inf_strategy}")
+        inf_caps = capacity_func()
+        self.simulate_multi_school_costs(inf_caps)
+        return self.evaluation_metrics(inf_caps)
 
 
-def simulate_2_school_costs(
-    true_cap1: int,
-    true_cap2: int,
-    inf_cap1: int,
-    inf_cap2: int,
-    co: float,
-    cu: float,
-    iters: int,
-    prob: float,
-) -> np.ndarray:
-    """
-    Function that estimates the true total cost corresponding to the system
-    of two schools for some decisions on the inflated capacities.
-    Returns the cost as an array with the first entry as the
-    overage and the second one as the underage cost.
-
-    :param true_cap1: true capacity of the first school
-    :param inf_cap1: inflated capacity at the first school
-    :param true_cap2: true capacity of the second school
-    :param inf_cap2: inflated capacity at the second school
-    :param co: per unit cost of overage
-    :param cu: per unit cost of underage
-    :param iters: number of iterations
-    :param prob: probability of dropout
-    :return: len 2 array where first entry is overage cost and second is underage over both schools
-    """
-    sch1_cost = simulate_yield_costs(true_cap1, inf_cap1, co, cu, iters, prob)
-    sch2_cost = simulate_yield_costs(true_cap2, inf_cap2, co, cu, iters, prob)
-    return sch1_cost + sch2_cost
-
-
-def optimize_2sch(
-    true_cap1: int, true_cap2: int, prob: float, iters: int, co: float, cu: float
-):
-    """
-    Function that performs a grid search optimizing for the best
-    pair of inflated capacities in terms of minimum total cost
-    to the system.
-
-    :param true_cap1: true capacity of the first school
-    :param true_cap2: true capacity of the second school
-    :param iters: number of iterations
-    :param prob: probability of dropout
-    :param co: unit overage cost
-    :param cu: unit underage cost
-    :return: np.ndarray containing costs from all inflated capacities in [q, q + qp] # TODO: Check that this is correct
-    """
-
-    # define intervals of possible amounts by which we can inflate by
-    # using qhat / (1-prob) <= q
-    int_length1 = int(true_cap1 * prob / (1 - prob))
-    int_length2 = int(true_cap2 * prob / (1 - prob))
-
-    # values we try for inflated capacities
-    values1 = np.arange(true_cap1, true_cap1 + int_length1 + 1)
-    values2 = np.arange(true_cap2, true_cap2 + int_length2 + 1)
-    avg_costs = np.zeros(shape=(int_length1, int_length2))
-
-    for qhat1 in values1:
-        for qhat2 in values2:
-            costs = simulate_2_school_costs(
-                true_cap1, true_cap2, qhat1, qhat2, co, cu, iters, prob
-            )
-            avg_costs[qhat1 - true_cap1, qhat2 - true_cap2] = costs[0] + costs[1]
-
-    return avg_costs
-
-
-def heuristic_set_capacity(total_cap: int, co: float, cu: float, prob: float):
-    """
-    Function that computes the total inflation given the heuristic we use
-    to construct a long binomial and look for the approximate inverse
-    of the CDF for the value equal to the critical ratio.
-    Returns an integer, the total inflation.
-
-    :total_cap: total capacity we are computing overage-underage costs with respect to
-    :co: per unit cost of overage
-    :cu: per unit cost of underage
-    :prob: probability of dropout
-    """
-
-    int_length = int(total_cap * prob / (1 - prob))
-    values = np.arange(total_cap, total_cap + int_length + 1)
-    ratio = float(co) / (cu + co)
-
-    best_val = values[0]
-    for qhat in values:
-        if binom.cdf(total_cap, qhat, 1 - prob) >= ratio:
-            best_val = qhat
-        else:
-            break
-    return best_val
-
-
-def compute_cdf_inflate_sep(
-    cap1: int, cap2: int, co: float, cu: float, prob: float
-) -> float:
-    """
-    Function that computes the CDF corresponding to the combined binomial,
-    resulting from adding up the inflated capacities, at the sum of true capacities.
-    Used to compare whether inflating separately results in more inflation than
-    when we are inflating school 2, school of interest, by viewing is as
-    integrated in the chain, as opposed to the top of its own chain or separately.
-    Returns a float, the value of CDF at the desired point.
-
-    :param cap1: capacity corresponding to the first binomial, so  either the first school or the chain up to our
-        school of interest
-    :param cap2: capacity
-    :param co: per unit cost of overage
-    :param cu: per unit cost of underage
-    :param prob: probability of dropout
-    :return: float with the value of the CDF at the desired point
-    """
-
-    # compute the inflated capacities when first school/chain and the second
-    # school-school of interest-inflate separately
-    qhat1 = heuristic_set_capacity(cap1, co, cu, prob)
-    qhat2 = heuristic_set_capacity(cap2, co, cu, prob)
-
-    # compute CDF at the sum of true capacities
-    return binom.cdf(cap1 + cap2, qhat1 + qhat2, 1 - prob)
-
-
-def simulate_multi_school_costs(true_caps, inf_caps, co, cu, iters, prob):
-    """
-    Function that estimates the true total costs corresponding to each of the
-    n schools for some decisions on the inflated capacities. Returns the result
-    as an n x 2 matrix, with the overage and underage cost corresponding to
-    each school as its rows.
-
-    :true_caps: true capacities of each school
-    :inf_caps: inflated capacities at of each school
-    :co: per unit cost of overage
-    :cu: per unit cost of underage
-    :iters: number of iterations
-    :prob: probability of dropout
-
-    """
-
-    n = len(true_caps)
-    costs = np.zeros((n, 2), dtype=float)
-
-    yld = np.random.binomial(inf_caps, 1-prob, size=(iters, n)).T
-    diff = yld - np.tile(true_caps, (iters, 1)).T
-
-    underfill_so_far = np.zeros(iters)
-    for k in range(n):
-        underfilled_mask = np.where(diff[k, :] < underfill_so_far, 1, 0)
-        overfilled_mask = np.where(diff[k, :] > underfill_so_far, 1, 0)
-        costs[k, 0] += co*np.sum(overfilled_mask*(diff[k, :] - underfill_so_far))
-        costs[k, 1] += cu*np.sum(underfilled_mask*((-1) * diff[k, :] + underfill_so_far))
-        remaining_underfill = underfill_so_far - overfilled_mask*diff[k, :] + underfilled_mask * (-1) * diff[k, :]
-        underfill_so_far = np.where(remaining_underfill > 0, remaining_underfill, 0)
-
-    costs = costs / iters
-    return costs
-
-
-def heuristic_set_capacity_chain(true_caps, co, cu, p):
-    running_true_cap = 0
-    running_inf_cap = 0
-    inf_caps = []
-    for q in true_caps:
-        running_true_cap += q
-        qhat = heuristic_set_capacity(running_true_cap, co, cu, p) - running_inf_cap
-        running_inf_cap += qhat
-        inf_caps.append(qhat)
-    return np.array(inf_caps)
-
-
-def heuristic_set_capacity_independent(true_caps, co, cu, p):
-    inf_caps = []
-    for q in true_caps:
-        qhat = heuristic_set_capacity(q, co, cu, p)
-        inf_caps.append(qhat)
-    return np.array(inf_caps)
--- capacity_management/tests/test_cost_simulation.py
+++ capacity_management/tests/test_cost_simulation.py
@@ -1,40 +1,41 @@
 import numpy as np
+import pytest
 
-from ..src.cost_simulation import (
-    simulate_yield_costs,
-    simulate_2_school_costs,
-    optimize_2sch,
-    heuristic_set_capacity,
-    compute_cdf_inflate_sep, simulate_multi_school_costs,
-)
+from ..src.cost_simulation import CostSimulator
 
 
-def test_simulate_yield_costs():
+@pytest.fixture
+def cost_simulator_obj():
+    return CostSimulator(10, 1, 1000, 0.15)
+
+
+def test_simulate_yield_costs(cost_simulator_obj):
     np.random.seed(0)
-    costs = simulate_yield_costs(40, 44, 10, 1, 1000, 0.15)
+    costs = cost_simulator_obj.simulate_yield_costs(40, 44)
     assert np.isclose(costs, [1.19, 2.714]).all()
 
 
-def test_simulate_2_school_costs():
+def test_simulate_2_school_costs(cost_simulator_obj):
     np.random.seed(0)
-    costs = simulate_2_school_costs(40, 60, 44, 67, 10, 1, 1000, 0.15)
+    costs = cost_simulator_obj.simulate_2_school_costs(40, 60, 44, 67)
     assert np.isclose(costs, [[3.24, 6.094]]).all()
 
 
 def test_optimize_2sch():
+    cs = CostSimulator(10, 1, 1, 0.15)
     np.random.seed(0)
-    cost_grid = optimize_2sch(12, 12, 0.15, 1, 10, 1)
+    cost_grid = cs.optimize_2sch(6, 6)
     assert cost_grid.shape == (2, 2)
-    assert np.isclose(cost_grid, [[4.0, 3.0], [3.0, 4.0]]).all()
+    assert np.isclose(cost_grid, [[2.0, 1.0], [1.0, 1.0]]).all()
 
 
-def test_heuristic_set_capacity():
-    best_cap = heuristic_set_capacity(40, 10, 1, 0.15)
+def test_heuristic_set_capacity(cost_simulator_obj):
+    best_cap = cost_simulator_obj.heuristic_set_capacity(40)
     assert best_cap == 44
 
 
-def test_compute_cdf_inflate_sep():
-    cdf_val = compute_cdf_inflate_sep(40, 40, 10, 1, 0.15)
+def test_compute_cdf_inflate_sep(cost_simulator_obj):
+    cdf_val = cost_simulator_obj.compute_cdf_inflate_sep(40, 40)
     assert np.isclose(cdf_val, 0.9631577914158603)
 
 
@@ -45,8 +46,9 @@ def test_simulate_multi_school_costs():
     co = cu = 1
     iters = 1
     prob = 0.15
+    cs = CostSimulator(co, cu, iters, prob, true_caps)
     np.random.seed(0)
-    costs = simulate_multi_school_costs(true_caps, inf_caps, co, cu, iters, prob)
+    cs.simulate_multi_school_costs(inf_caps)
     expected = np.array([[1., 0.],
        [0., 0.],
        [0., 3.],
@@ -54,4 +56,4 @@ def test_simulate_multi_school_costs():
        [0., 1.],
        [0., 2.],
        [0., 2.]])
-    assert np.isclose(costs, expected).all()
+    assert np.isclose(cs.costs, expected).all()
